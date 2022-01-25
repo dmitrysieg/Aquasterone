52cddbfc75b5a9541d2d7804c1c714631ec342a8
Dmitry Sieg <dmitry.sieg@gmail.com>
2022-01-25 03:25:19 +0300

    correct random position generation

--- js/aquatic.js
+++ js/aquatic.js
@@ -6,8 +6,8 @@ let Aquatic = function(x, y, value) {
     this.el.className = 'aquatic';
     this.el.style.height = this.height;
     this.el.style.width = this.width;
-    this.el.style.left = x;
-    this.el.style.top = y;
+    this.el.style.left = x - this.width / 2;
+    this.el.style.top = y - this.height / 2;
     document.body.appendChild(this.el);
 }
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 (function() {
-    let x1 = Math.random() * document.body.clientWidth;
-    let y1 = Math.random() * document.body.clientHeight;
-    let a1 = new Aquatic(x1, y1);
+    let x1 = (Aquatic.prototype.width / 2) + Math.random() * (document.body.clientWidth - Aquatic.prototype.width);
+    let y1 = (Aquatic.prototype.height / 2) + Math.random() * (document.body.clientHeight - Aquatic.prototype.height);
+    let value1 = Math.random();
+    let a1 = new Aquatic(x1, y1, value1);
 })();
